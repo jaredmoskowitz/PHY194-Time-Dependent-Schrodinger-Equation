807d76621ff674ab08ee2f2d7640f58614640410
Jared Moskowitz <jaredmoskowitz@me.com>
2015-04-03 14:11:27 -0400

    reasonabble psi values

--- schrodinger.py
+++ schrodinger.py
@@ -9,31 +9,29 @@ boundaryConditions = (-45, 45)
 totalSteps = int((boundaryConditions[1] - boundaryConditions[0])/dx)
 
 def finiteDifferenceEquation(psi, V):
+
         systemMatrix = np.zeros((totalSteps, totalSteps), complex) #matrix for system of eq
-        u = -1
         x = boundaryConditions[0]
-        systemVector = psi
 
-
+        coeff =  dt/(1j*(dx ** 2))
         #create system of equations x
         for i in range(totalSteps):
-                systemMatrix[i, u%totalSteps] = dt/(1j*(dx ** 2))
-                systemMatrix[i, (u + 1)%totalSteps] = V(x + dx) - 2*dt/(1j*(dx ** 2))
-                systemMatrix[i, (u + 2)%totalSteps] = dt/(1j*(dx ** 2))
+                systemMatrix[i, (i - 1)%totalSteps] = coeff
+                systemMatrix[i, (i)%totalSteps] = (V(x)*(dx ** 2) - 2/(dx ** 2))*coeff
+                systemMatrix[i, (i + 1)%totalSteps] = coeff
 
-                systemVector[i] = psi[i]
-                u += 1
                 x += dx
 
-        return np.linalg.solve(systemMatrix, systemVector)
+        return np.linalg.solve(systemMatrix, psi)
 
 
 def generateWavePacket( x0, k0, sigma):
          psi = lambda x: np.exp(.25*(x-x0)*complex((x0-x)*sigma ** 2, 4*k0)*np.sqrt(np.pi)*sigma/np.sqrt(np.sqrt(2)*np.pi ** 3/2 * sigma))
-         return [psi((x*dx+boundaryConditions[0])) for x in range(totalSteps)]
+         return np.array([psi((x*dx+boundaryConditions[0])) for x in range(totalSteps)])
 
 def main():
         ps = generateWavePacket(float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3]))
+        print ps[:10]
         potential = helpers.flatWell
         for i in range(10):
                 ps = finiteDifferenceEquation(ps, potential)
